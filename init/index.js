918e86831a2787f217b5e245b011b03cb8b6a638
sachin624k <sachin77000k@gmail.com>
2025-12-07 16:05:58 +0530

    Initial commit

---
+++ init/index.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const initData = require("./data");
+const Listing = require("../models/listing.js");
+
+const MNOGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+
+main()
+  .then(() => {
+    console.log("Connected to DB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/wanderlust');
+}
+
+const initDB = async () => {
+    await Listing.deleteMany({});
+    await Listing.insertMany(initData.data);
+    console.log("Data was initialized");
+};
+
+initDB();
